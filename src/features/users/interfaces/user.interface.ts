3d0b80e282ef6c7070e03dcb3868df07c52b8fde
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 19:07:31 -0300

    feat(users): user interface

---
+++ src/features/users/interfaces/user.interface.ts
@@ -0,0 +1,17 @@
+import type { IRole } from "@roles/interfaces/role.interface";
+
+export interface IUser {
+  createdAt: string;
+  deletedAt?: string;
+  email: string;
+  firstName: string;
+  ic: string;
+  id: string;
+  lastName: string;
+  password: string;
+  phoneNumber: string;
+  role: IRole;
+  roleId: string;
+  updatedAt: string;
+  userName: string;
+}
