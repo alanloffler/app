f1e334d110160a69dc006e48379b659237b74bd3
alanloffler <alanmatiasloffler@gmail.com>
2026-01-04 23:15:52 -0300

    refactor(login): renamed for user login

--- src/features/login/UserLogin.tsx
+++ src/features/login/UserLogin.tsx
@@ -1,9 +1,9 @@
 import { LoginForm } from "@login/components/LoginForm";
 
-export function Login() {
+export function UserLogin() {
   return (
     <div className="flex h-screen w-full items-center justify-center p-4 sm:p-6 md:p-10">
-      <LoginForm />
+      <LoginForm type="user" />
     </div>
   );
 }
