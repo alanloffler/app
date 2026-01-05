66e715e4eb54790c0370d546b065934e9a7b11e3
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 20:39:58 -0300

    feat(users): page header, create cta

--- src/features/users/Users.tsx
+++ src/features/users/Users.tsx
@@ -1,3 +1,22 @@
+import { Protected } from "@/core/auth/components/Protected";
+import { PageHeader } from "@/core/components/pages/PageHeader";
+import { Button } from "@/core/components/ui/button";
+import { Plus } from "lucide-react";
+import { Link } from "react-router";
+
 export default function Users() {
-  return <>Página de Usuarios</>;
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeader title="Pacientes" subtitle="Gestioná los pacientes del sistema">
+        <Protected requiredPermission="users-create">
+          <Button variant="default" size="lg" asChild>
+            <Link to="/users/create">
+              <Plus />
+              Crear paciente
+            </Link>
+          </Button>
+        </Protected>
+      </PageHeader>
+    </div>
+  );
 }
