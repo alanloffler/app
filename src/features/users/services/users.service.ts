c3778ec588408b49282de49c49cc67f456d8d57e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 21:12:12 -0300

    feat(users): add services

--- src/features/users/services/users.service.ts
+++ src/features/users/services/users.service.ts
@@ -17,6 +17,29 @@ class UsersModuleService {
     const response = await apiClient.post("/users", data);
     return response.data;
   }
+
+  public async findAll(): Promise<IApiResponse<IUser[]>> {
+    const response = await apiClient.get("/users");
+    return response.data;
+  }
+
+  // TODO: Implement real endpoint to include those users soft removed
+  public async findAllSoftRemoved(): Promise<IApiResponse<IUser[]>> {
+    const response = await apiClient.get("/users");
+    return response.data;
+  }
+
+  public async findOne(id: string): Promise<IApiResponse<IUser>> {
+    const response = await apiClient.get(`/users/${id}`);
+    return response.data;
+  }
+
+  // TODO: Implement real endpoint for soft removed
+  public async findOneSoftRemoved(id: string): Promise<IApiResponse<IUser>> {
+    // const response = await apiClient.get(`/users/${id}/soft-removed`);
+    const response = await apiClient.get(`/users/${id}`);
+    return response.data;
+  }
 }
 
 export const UsersService = UsersModuleService.getInstance();
