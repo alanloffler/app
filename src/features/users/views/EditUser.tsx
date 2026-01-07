af7edaa40a4b72c9744ed2559c38ecd396050cbc
alanloffler <alanmatiasloffler@gmail.com>
2026-01-07 15:50:33 -0300

    feat(users): edit form

--- src/features/users/views/EditUser.tsx
+++ src/features/users/views/EditUser.tsx
@@ -1,3 +1,13 @@
+import { EditForm } from "@users/components/EditForm";
+
+import { useParams } from "react-router";
+
 export default function EditUser() {
-  return <>User edit view</>;
+  const { id } = useParams();
+
+  return (
+    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
+      <EditForm userId={id!} />
+    </div>
+  );
 }
