0c2439a09e8b94322c5d9c5e3f5fdd959abefec2
alanloffler <alanmatiasloffler@gmail.com>
2026-01-07 22:31:27 -0300

    build: calendar alias

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       "@account": path.resolve(__dirname, "./src/features/account"),
       "@admin": path.resolve(__dirname, "./src/features/admin"),
       "@auth": path.resolve(__dirname, "./src/core/auth"),
+      "@calendar": path.resolve(__dirname, "./src/core/calendar"),
       "@components": path.resolve(__dirname, "./src/core/components"),
       "@dashboard": path.resolve(__dirname, "./src/features/dashboard"),
       "@core": path.resolve(__dirname, "./src/core"),
