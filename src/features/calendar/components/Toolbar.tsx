50cbc36a62a83ba48a639777dcd86703e471481e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-08 20:59:09 -0300

    feat(toolbar): responsive classes

--- src/features/calendar/components/Toolbar.tsx
+++ src/features/calendar/components/Toolbar.tsx
@@ -4,14 +4,23 @@ import { Navigation } from "@calendar/components/Navigation";
 import type { ToolbarProps } from "react-big-calendar";
 
 import type { CalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@components/ui/sidebar";
 
 interface IProps extends ToolbarProps<CalendarEvent> {
   currentDate: Date;
 }
 
 export function Toolbar(props: IProps) {
+  const { open } = useSidebar();
+
   return (
-    <div className="flex items-center justify-between rounded-t-lg border border-b-0 p-6">
+    <div
+      className={cn(
+        "flex flex-col items-center justify-between gap-4 rounded-t-lg border border-b-0 p-6 lg:flex-row",
+        open ? "md:flex-col" : "md:flex-row",
+      )}
+    >
       <DateHeader currentDate={props.currentDate} />
       <Navigation {...props} />
     </div>
