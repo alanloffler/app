624fdc64d97b3f78f7422c4f824f27164e5e59cd
alanloffler <alanmatiasloffler@gmail.com>
2026-01-09 14:01:17 -0300

    feat(calendar): userId prop schema

--- src/features/calendar/schemas/event.schema.ts
+++ src/features/calendar/schemas/event.schema.ts
@@ -2,4 +2,5 @@ import z from "zod";
 
 export const eventSchema = z.object({
   title: z.string().nonempty("El título es obligatorio"),
+  userId: z.string().nonempty("El usuario es obligatorio"),
 });
