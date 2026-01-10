0113e970874fe75cf9c95c2a62e8f25c069ecca9
alanloffler <alanmatiasloffler@gmail.com>
2026-01-09 21:07:06 -0300

    refactor(calendar): update validations

--- src/features/calendar/schemas/event.schema.ts
+++ src/features/calendar/schemas/event.schema.ts
@@ -1,6 +1,19 @@
 import z from "zod";
 
 export const eventSchema = z.object({
-  title: z.string().nonempty("El título es obligatorio"),
-  userId: z.string().nonempty("El usuario es obligatorio"),
+  date: z
+    .string()
+    .nonempty("La fecha y horario son obligatorios")
+    .refine(
+      (val) => {
+        if (!val) return false;
+        const date = new Date(val);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        return hours !== 0 || minutes !== 0;
+      },
+      { message: "Debe seleccionar un horario" },
+    ),
+  title: z.string().nonempty("El título es obligatorio").min(3, "El título debe tener al menos 3 caracteres"),
+  userId: z.uuid("Formato de UUID inválido").nonempty("El paciente es obligatorio"),
 });
