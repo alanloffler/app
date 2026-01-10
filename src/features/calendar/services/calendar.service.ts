20e20e7275c465da9ce9fa5ba93a686ea6134517
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 20:15:16 -0300

    feat(calendar): find all events from database

--- src/features/calendar/services/calendar.service.ts
+++ src/features/calendar/services/calendar.service.ts
@@ -1,6 +1,6 @@
 import type { IApiResponse } from "@core/interfaces/api-response.interface";
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
-// import { apiClient } from "@core/client/client";
+import { apiClient } from "@core/client/client";
 
 class CalendarModuleService {
   private static instance: CalendarModuleService;
@@ -14,58 +14,23 @@ class CalendarModuleService {
   }
 
   public async findAll(): Promise<IApiResponse<ICalendarEvent[]>> {
-    const events: ICalendarEvent[] = [
-      {
-        id: 0,
-        title: "Event 1",
-        start: new Date("2026-01-07T10:00:00"),
-        end: new Date("2026-01-07T10:30:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-      {
-        id: 1,
-        title: "Event 2",
-        start: new Date("2026-01-07T10:30:00"),
-        end: new Date("2026-01-07T11:00:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-      {
-        id: 2,
-        title: "Event 3",
-        start: new Date("2026-01-08T10:00:00"),
-        end: new Date("2026-01-08T10:30:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-      {
-        id: 3,
-        title: "Event 4",
-        start: new Date("2026-01-08T11:00:00"),
-        end: new Date("2026-01-08T11:30:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-      {
-        id: 4,
-        title: "Event 5",
-        start: new Date("2026-01-08T11:30:00"),
-        end: new Date("2026-01-08T12:00:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-      {
-        id: 5,
-        title: "Event 6",
-        start: new Date("2026-01-08T13:00:00"),
-        end: new Date("2026-01-08T13:30:00"),
-        resourceId: 1,
-        userId: "439a4db0-d527-473e-9566-27aee5fcbd35",
-      },
-    ];
+    const response = await apiClient.get("/events");
+    const data = response.data;
+    if (!data.data) return data;
 
-    return { message: "Eventos encontrados", statusCode: 200, data: events };
+    return { ...data, data: this.toDate(data.data) };
+  }
+
+  private toDate(events: ICalendarEvent[]) {
+    if (events) {
+      return events.map((event: any) => ({
+        ...event,
+        startDate: new Date(event.startDate),
+        endDate: new Date(event.endDate),
+      }));
+    }
+
+    return events;
   }
 }
 
