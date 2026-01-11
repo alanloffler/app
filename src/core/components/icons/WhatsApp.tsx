34c40fcffddae35889aee91d2530a6939d8b3383
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 23:19:40 -0300

    feat(icons): icon origin

--- src/core/components/icons/WhatsApp.tsx
+++ src/core/components/icons/WhatsApp.tsx
@@ -1,3 +1,5 @@
+// https://simpleicons.org/
+
 export function WhatsApp({ className }: { className?: string }) {
   return (
     <svg className={className} width="14" height="14" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
