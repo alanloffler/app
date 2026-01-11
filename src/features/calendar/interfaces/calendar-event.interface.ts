ad78c5116e89165bc29c5256060ab30c1eed987e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 21:22:35 -0300

    feat(calendar): user prop

--- src/features/calendar/interfaces/calendar-event.interface.ts
+++ src/features/calendar/interfaces/calendar-event.interface.ts
@@ -1,8 +1,11 @@
+import type { IUserEvent } from "@calendar/interfaces/user-event.interface";
+
 export interface ICalendarEvent {
   endDate: Date;
   id: number;
   resourceId: number;
   startDate: Date;
   title: string;
+  user: IUserEvent;
   userId: string;
 }
