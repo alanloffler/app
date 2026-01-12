25e9752440e01b2e03e1cdb5a1fa6959c2c4d078
alanloffler <alanmatiasloffler@gmail.com>
2026-01-11 21:57:49 -0300

    feat(calendar): calendarView prop

--- src/features/calendar/components/Toolbar.tsx
+++ src/features/calendar/components/Toolbar.tsx
@@ -5,10 +5,12 @@ import { Navigation } from "@calendar/components/Navigation";
 import type { ToolbarProps } from "react-big-calendar";
 
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+import type { TView } from "@calendar/interfaces/calendar-view.type";
 import { cn } from "@lib/utils";
 import { useSidebar } from "@components/ui/sidebar";
 
 interface IProps extends ToolbarProps<ICalendarEvent> {
+  calendarView: TView;
   currentDate: Date;
   onCreateEvent: () => void;
 }
@@ -23,7 +25,7 @@ export function Toolbar(props: IProps) {
         open ? "md:flex-col" : "md:flex-row",
       )}
     >
-      <DateHeader currentDate={props.currentDate} />
+      <DateHeader calendarView={props.calendarView} currentDate={props.currentDate} />
       <div className="flex items-center gap-3">
         <Navigation {...props} />
         <AddEvent onCreateEvent={props.onCreateEvent} />
