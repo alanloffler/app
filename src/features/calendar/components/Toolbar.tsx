c430f5546625bc9202b7e73d62636e556edcc74c
alanloffler <alanmatiasloffler@gmail.com>
2026-01-12 13:57:13 -0300

    feat(calendar): responsive layout style

--- src/features/calendar/components/Toolbar.tsx
+++ src/features/calendar/components/Toolbar.tsx
@@ -26,7 +26,7 @@ export function Toolbar(props: IProps) {
       )}
     >
       <DateHeader calendarView={props.calendarView} currentDate={props.currentDate} />
-      <div className="flex items-center gap-3">
+      <div className="flex flex-col items-center gap-3 md:flex-row">
         <Navigation {...props} />
         <AddEvent onCreateEvent={props.onCreateEvent} />
       </div>
