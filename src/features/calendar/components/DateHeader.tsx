0a8dd9166384b5074a5ada116672391294a18a37
alanloffler <alanmatiasloffler@gmail.com>
2026-01-12 20:01:44 -0300

    feat(calendar): styles matches dark/light

--- src/features/calendar/components/DateHeader.tsx
+++ src/features/calendar/components/DateHeader.tsx
@@ -37,22 +37,22 @@ export function DateHeader({ calendarView, currentDate }: IProps) {
   return (
     <div className="flex items-center gap-3">
       <div className="inline-flex min-w-16 flex-col items-center rounded-lg border">
-        <span className="w-full rounded-t-lg bg-gray-50 px-2 py-1 text-center text-xs font-semibold text-gray-500 uppercase">
+        <span className="bg-accent text-muted-foreground w-full rounded-t-lg px-2 py-1 text-center text-xs font-semibold uppercase">
           {monthShort}
         </span>
         <span className="text-primary text-lg font-bold">{day}</span>
       </div>
       <div className="flex flex-col gap-1">
         <div className="flex items-center gap-2">
-          <span className="text-lg font-semibold text-gray-900 capitalize">
+          <span className="text-foreground text-lg font-semibold capitalize">
             {month} {year}
           </span>
-          <div className="rounded-sm border px-1.5 py-0.5 text-xs font-medium text-gray-700">Semana {week}</div>
+          <div className="text-muted-foreground rounded-sm border px-1.5 py-0.5 text-xs font-medium">Semana {week}</div>
         </div>
         {calendarView === "day" ? (
-          <div className="text-sm text-gray-600 capitalize">{dayString}</div>
+          <div className="text-muted-foreground text-sm capitalize">{dayString}</div>
         ) : (
-          <div className="text-sm text-gray-600 capitalize">
+          <div className="text-muted-foreground text-sm capitalize">
             <span>{`${firstDay} ${monthShort} ${year} - ${lastDay} ${monthShort} ${year}`}</span>
           </div>
         )}
