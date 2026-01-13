c62e4b8f25e25600ef55ec90972c2844d0d7984a
alanloffler <alanmatiasloffler@gmail.com>
2026-01-13 18:40:39 -0300

    feat(calendar): edit event sheet

---
+++ src/features/calendar/components/EditEvent.tsx
@@ -0,0 +1,26 @@
+import type { Dispatch, SetStateAction } from "react";
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@components/ui/sheet";
+
+import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+
+interface IProps {
+  event: ICalendarEvent | null;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export function EditEvent({ event, open, setOpen }: IProps) {
+  return (
+    <Sheet open={event !== null && open} onOpenChange={setOpen}>
+      <SheetTrigger asChild></SheetTrigger>
+      <SheetContent className="sm:min-w-[480px]" onOpenAutoFocus={(e) => e.preventDefault()}>
+        <SheetHeader className="pt-8">
+          <SheetTitle className="text-lg">Edición del turno</SheetTitle>
+          <SheetDescription className="text-base">Formulario para la edición del turno seleccionado</SheetDescription>
+        </SheetHeader>
+        <div className="flex flex-col gap-6 p-4">{JSON.stringify(event, null, 2)}</div>
+      </SheetContent>
+    </Sheet>
+  );
+}
