feea25136256bd331917bf44a0e9e6012dfb683e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-13 23:19:08 -0300

    feat(calendar): persist states store

---
+++ src/features/calendar/stores/calendar.store.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import type { TView } from "@calendar/interfaces/calendar-view.type";
+
+interface CalendarState {
+  selectedDate: Date;
+  selectedView: TView;
+  setSelectedDate: (date: Date) => void;
+  setSelectedView: (view: TView) => void;
+}
+
+export const useCalendarStore = create(
+  persist<CalendarState>(
+    (set) => ({
+      selectedDate: new Date(),
+      selectedView: "month",
+      setSelectedDate: (date) => set({ selectedDate: date }),
+      setSelectedView: (view) => set({ selectedView: view }),
+    }),
+    {
+      name: "calendar",
+      storage: createJSONStorage(() => sessionStorage),
+      merge: (persistedState, currentState) => ({
+        ...currentState,
+        ...(persistedState as CalendarState),
+        selectedDate:
+          (persistedState as CalendarState)?.selectedDate
+            ? new Date((persistedState as CalendarState).selectedDate)
+            : currentState.selectedDate,
+      }),
+    },
+  ),
+);
