6af4acc9f513f4580b04cfa3bdbccc0cd849da29
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 16:15:05 -0300

    feat(auth): protected can handle TPermission | TPermission[]

--- src/core/auth/components/Protected.tsx
+++ src/core/auth/components/Protected.tsx
@@ -1,22 +1,33 @@
 import type { ReactNode } from "react";
 
 import type { TPermission } from "@permissions/interfaces/permission.type";
-import { usePermission } from "@permissions/hooks/usePermission";
+import { useAuthStore } from "@auth/stores/auth.store";
 
 interface IProps {
   children: ReactNode;
-  requiredPermission: TPermission;
+  mode?: "some" | "every";
+  requiredPermission?: TPermission | TPermission[];
   variant?: "invisible" | "disabled";
 }
 
-export function Protected({ children, requiredPermission, variant = "invisible" }: IProps) {
-  const hasPermission = usePermission(requiredPermission);
+export function Protected({ children, mode = "some", requiredPermission, variant = "invisible" }: IProps) {
+  const admin = useAuthStore((state) => state.admin);
+  const adminPermissions = admin?.role.rolePermissions ?? [];
 
-  if (requiredPermission === "*" || !requiredPermission) {
+  const required = Array.isArray(requiredPermission)
+    ? requiredPermission
+    : requiredPermission
+      ? [requiredPermission]
+      : [];
+
+  if (required.length === 0 || required.includes("*")) {
     return children;
   }
 
-  if (!hasPermission) {
+  const hasPermission = (r: TPermission) => adminPermissions.some((p) => p.permission?.actionKey === r);
+  const hasRequiredPermissions = mode === "every" ? required.every(hasPermission) : required.some(hasPermission);
+
+  if (!hasRequiredPermissions) {
     if (variant === "disabled") {
       return <div className="contents *:pointer-events-none *:cursor-not-allowed *:opacity-50">{children}</div>;
     }
