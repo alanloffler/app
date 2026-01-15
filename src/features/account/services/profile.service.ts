74d7717b887c644307f448e0109505aad5c0acf3
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 19:29:49 -0300

    refactor(account): revert get profile by user role

--- src/features/account/services/profile.service.ts
+++ src/features/account/services/profile.service.ts
@@ -13,22 +13,14 @@ class AccountModuleService {
     return AccountModuleService.instance;
   }
 
-  public async get(userRole?: string): Promise<IApiResponse<IAdmin>> {
-    let path: string = "/users/profile";
-
-    if (userRole === "superadmin") {
-      path = "/admin/profile";
-    }
-
-    const response = await apiClient.get(path);
+  public async get(): Promise<IApiResponse<IAdmin>> {
+    const response = await apiClient.get("/users/profile");
     return response.data;
   }
 
   public async update(data: Partial<IAdmin>): Promise<IApiResponse<IAdmin>> {
-    console.log(data);
-
-    // const response = await apiClient.patch("/admin/profile", data);
-    // return response.data;
+    const response = await apiClient.patch("/users/profile", data);
+    return response.data;
   }
 }
 
