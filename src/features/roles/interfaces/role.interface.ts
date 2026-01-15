0e8705509c63c8631ed375416524a108d1c2f405
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 12:31:16 -0300

    feat(roles): users prop extend interface

--- src/features/roles/interfaces/role.interface.ts
+++ src/features/roles/interfaces/role.interface.ts
@@ -1,14 +1,16 @@
 import type { IAdmin } from "@admin/interfaces/admin.interface";
 import type { IPermission } from "@permissions/interfaces/permission.interface";
+import type { IUser } from "@users/interfaces/user.interface";
 
 export interface IRole {
-  admins: Pick<IAdmin, "id" | "firstName" | "lastName" | "userName">[];
+  admins: Pick<IAdmin, "id" | "firstName" | "lastName" | "role" | "userName">[];
   createdAt: string;
   deletedAt?: string;
   description: string;
   id: string;
   name: string;
   rolePermissions?: IRolePermissions[];
+  users: Pick<IUser, "id" | "firstName" | "lastName" | "role" | "userName">[];
   value: string;
 }
 
