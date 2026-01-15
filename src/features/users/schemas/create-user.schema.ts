185f8a35529d5a57e992b734f6a4e170e64b06b0
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 20:37:12 -0300

    refactor(users): update message

--- src/features/users/schemas/create-user.schema.ts
+++ src/features/users/schemas/create-user.schema.ts
@@ -5,6 +5,6 @@ import { userSchema } from "@users/schemas/users.schema";
 export const createUserSchema = userSchema.extend({
   password: z
     .string()
-    .nonempty("La contraseña no puede estar vacía")
+    .nonempty("La contraseña es obligatoria")
     .min(8, "La contraseña debe tener al menos 8 caracteres"),
 });
