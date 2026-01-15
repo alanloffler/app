1b0c08f1795cd4726bebce0d16aa2364659a8356
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 20:37:00 -0300

    feat(users): update phoneNumber validation

--- src/features/users/schemas/users.schema.ts
+++ src/features/users/schemas/users.schema.ts
@@ -2,17 +2,16 @@ import { z } from "zod";
 
 export const userSchema = z.object({
   email: z.email({ message: "Debes ingresar un email válido" }),
-  firstName: z.string().nonempty("El nombre no puede estar vacío"),
-  ic: z
+  firstName: z.string().nonempty("El nombre es obligatorio"),
+  ic: z.string().nonempty("El número de DNI es obligatorio").max(8, "Máximo 8 dígitos").min(8, "Mínimo 8 dígitos"),
+  lastName: z.string().nonempty("El apellido es obligatorio"),
+  phoneNumber: z
     .string()
-    .nonempty("El número de DNI no puede estar vacío")
-    .max(8, "Máximo 8 dígitos")
-    .min(8, "Mínimo 8 dígitos"),
-  lastName: z.string().nonempty("El apellido no puede estar vacío"),
-  phoneNumber: z.string().nonempty("El número de teléfono no puede estar vacío"),
-  roleId: z.string().nonempty("El rol no puede estar vacío"),
+    .nonempty("El número de teléfono es obligatorio")
+    .length(10, "El número de teléfono debe tener 10 dígitos"),
+  roleId: z.string().nonempty("El rol es obligatorio"),
   userName: z
     .string()
-    .min(2, "El nombre de usuario no puede estar vacío")
+    .min(2, "El nombre de usuario es obligatorio")
     .regex(/^@.{3,}$/, { message: "Mínimo 3 caracteres" }),
 });
