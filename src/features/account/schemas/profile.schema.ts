9bbf22ccdd55c3f66d6704b89fc1136236ee62df
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 21:07:24 -0300

    feat(account): update phoneNumber validation & messages

--- src/features/account/schemas/profile.schema.ts
+++ src/features/account/schemas/profile.schema.ts
@@ -2,17 +2,16 @@ import { z } from "zod";
 
 export const profileSchema = z.object({
   email: z.email({ message: "Debes ingresar un email válido" }),
-  firstName: z.string().nonempty("El nombre no puede estar vacío"),
-  ic: z
-    .string()
-    .nonempty("El número de DNI no puede estar vacío")
-    .max(8, "Máximo 8 dígitos")
-    .min(8, "Mínimo 8 dígitos"),
-  lastName: z.string().nonempty("El apellido no puede estar vacío"),
+  firstName: z.string().nonempty("El nombre es obligatorio"),
+  ic: z.string().nonempty("El número de DNI es obligatorio").max(8, "Máximo 8 dígitos").min(8, "Mínimo 8 dígitos"),
+  lastName: z.string().nonempty("El apellido es obligatorio"),
   password: z
     .string()
     .optional()
     .refine((val) => !val || val.length >= 8, "La contraseña debe tener al menos 8 caracteres"),
-  phoneNumber: z.string().nonempty("El número de teléfono no puede estar vacío"),
-  userName: z.string().nonempty("El nombre de usuario no puede estar vacío"),
+  phoneNumber: z
+    .string()
+    .nonempty("El número de teléfono es obligatorio")
+    .length(10, "El número de teléfono debe tener 10 dígitos"),
+  userName: z.string().nonempty("El nombre de usuario es obligatorio"),
 });
