e6562af8363385c29a21bc148076a22de356d348
alanloffler <alanmatiasloffler@gmail.com>
2026-01-19 20:48:59 -0300

    feat(business): patients card

---
+++ src/features/business/components/PatientsCard.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@components/ui/button";
+import { Card, CardContent } from "@components/ui/card";
+import { Link } from "react-router";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table";
+
+import { es } from "date-fns/locale";
+import { format } from "date-fns";
+
+import type { IUser } from "@users/interfaces/user.interface";
+
+interface IProps {
+  patients: IUser[];
+  className?: string;
+}
+
+export function PatientsCard({ patients, className }: IProps) {
+  return (
+    <Card className={className}>
+      <CardContent className="flex flex-1 flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-lg font-semibold">Últimos pacientes</h1>
+          <Button size="sm" variant="outline" asChild>
+            <Link to="/users/role/patient">Ver todos</Link>
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[60px]">ID</TableHead>
+              <TableHead>Paciente</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Fecha de alta</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {patients.map((patient) => (
+              <TableRow key={patient.id}>
+                <TableCell className="font-medium">{patient.id.slice(0, 4)}</TableCell>
+                <TableCell>{`${patient.firstName} ${patient.lastName}`}</TableCell>
+                <TableCell>{patient.email}</TableCell>
+                <TableCell>{format(patient.createdAt, "P", { locale: es })}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
