397ce5a0e76e8fc9c4bea982dc00811f1fe60700
alanloffler <alanmatiasloffler@gmail.com>
2026-01-19 18:41:12 -0300

    build: business alias

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       "@account": path.resolve(__dirname, "./src/features/account"),
       "@admin": path.resolve(__dirname, "./src/features/admin"),
       "@auth": path.resolve(__dirname, "./src/core/auth"),
+      "@business": path.resolve(__dirname, "./src/features/business"),
       "@calendar": path.resolve(__dirname, "./src/features/calendar"),
       "@components": path.resolve(__dirname, "./src/core/components"),
       "@dashboard": path.resolve(__dirname, "./src/features/dashboard"),
