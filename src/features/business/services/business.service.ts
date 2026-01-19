2123fbb1a636a38c5ae6231c7756ec77ee3b8592
alanloffler <alanmatiasloffler@gmail.com>
2026-01-19 20:15:32 -0300

    feat(business): service

---
+++ src/features/business/services/business.service.ts
@@ -0,0 +1,22 @@
+import type { IApiResponse } from "@core/interfaces/api-response.interface";
+import type { IBusiness } from "@business/interfaces/business.interface";
+import { apiClient } from "@core/client/client";
+
+class BusinessModuleService {
+  private static instance: BusinessModuleService;
+
+  public static getInstance(): BusinessModuleService {
+    if (!BusinessModuleService.instance) {
+      BusinessModuleService.instance = new BusinessModuleService();
+    }
+
+    return BusinessModuleService.instance;
+  }
+
+  public async findOne(id: string): Promise<IApiResponse<IBusiness>> {
+    const response = await apiClient.get(`/business/${id}`);
+    return response.data;
+  }
+}
+
+export const BusinessService = BusinessModuleService.getInstance();
