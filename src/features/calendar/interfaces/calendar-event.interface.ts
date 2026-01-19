ae6005636072165733ae18ba99739d5ee8d4ab16
alanloffler <alanmatiasloffler@gmail.com>
2026-01-18 22:45:12 -0300

    feat(calendar): professional prop

--- src/features/calendar/interfaces/calendar-event.interface.ts
+++ src/features/calendar/interfaces/calendar-event.interface.ts
@@ -1,8 +1,10 @@
+import type { IProfessionalEvent } from "@calendar/interfaces/professional-event.interface";
 import type { IUserEvent } from "@calendar/interfaces/user-event.interface";
 
 export interface ICalendarEvent {
   endDate: Date;
   id: string;
+  professional: IProfessionalEvent;
   professionalId: string;
   startDate: Date;
   title: string;
