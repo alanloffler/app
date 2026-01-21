633a443cd21f48bc88bf637e32501f8815efbf31
alanloffler <alanmatiasloffler@gmail.com>
2026-01-20 21:26:29 -0300

    feat(business): complete form

--- src/features/settings/BusinessSettings.tsx
+++ src/features/settings/BusinessSettings.tsx
@@ -1,3 +1,420 @@
+import { Button } from "@components/ui/button";
+import { Card, CardContent, CardFooter } from "@components/ui/card";
+import { Controller } from "react-hook-form";
+import { FieldGroup, Field, FieldLabel, FieldError } from "@components/ui/field";
+import { Input } from "@components/ui/input";
+import { Loader } from "@components/Loader";
+import { PageHeader } from "@components/pages/PageHeader";
+
+import type z from "zod";
+import { toast } from "sonner";
+import { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { BusinessService } from "@business/services/business.service";
+import { businessSchema } from "@business/schemas/business.schema";
+import { cn } from "@lib/utils";
+import { useSidebar } from "@components/ui/sidebar";
+import { useTryCatch } from "@core/hooks/useTryCatch";
+
 export default function BusinessSettings() {
-  return <div>BusinessSettings</div>;
+  const navigate = useNavigate();
+  const { open } = useSidebar();
+  const { isLoading: isSaving, tryCatch: tryCatchBusiness } = useTryCatch();
+
+  const form = useForm<z.infer<typeof businessSchema>>({
+    resolver: zodResolver(businessSchema),
+    defaultValues: {
+      slug: "",
+      taxId: "",
+      companyName: "",
+      tradeName: "",
+      description: "",
+      street: "",
+      city: "",
+      province: "",
+      country: "",
+      zipCode: "",
+      email: "",
+      phoneNumber: "",
+      whatsAppNumber: "",
+      website: "",
+    },
+  });
+
+  const fetchBusiness = useCallback(async (): Promise<void> => {
+    const [response, error] = await tryCatchBusiness(BusinessService.findOne());
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    if (response && response.statusCode === 200 && response.data) {
+      if (response.data) {
+        const r = response.data;
+
+        form.reset({
+          slug: r.slug,
+          taxId: r.taxId,
+          companyName: r.companyName,
+          tradeName: r.tradeName,
+          description: r.description,
+          street: r.street,
+          city: r.city,
+          province: r.province,
+          country: r.country,
+          zipCode: r.zipCode,
+          email: r.email,
+          phoneNumber: r.phoneNumber,
+          whatsAppNumber: r.whatsAppNumber,
+        });
+      }
+    }
+  }, [form, tryCatchBusiness]);
+
+  useEffect(() => {
+    fetchBusiness();
+  }, [fetchBusiness]);
+
+  function onSubmit(data: z.infer<typeof businessSchema>): void {
+    console.log(data);
+  }
+
+  function handleCancel(): void {
+    form.reset();
+    navigate(-1);
+  }
+
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeader title="Configuraciones de tu negocio" />
+      <Card className="w-full 2xl:max-w-[1100px]">
+        <CardContent className="flex flex-col gap-6">
+          <form className="grid grid-cols-1 gap-6 xl:grid-cols-2" id="edit-form" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col">
+                <h2 className="text-lg font-semibold">Datos comerciales</h2>
+                <span className="text-muted-foreground text-sm">Actualiza la información comercial de tu negocio</span>
+              </div>
+              <FieldGroup
+                className={cn("grid grid-cols-1 gap-6", open ? "md:grid-cols-1 lg:grid-cols-12" : "md:grid-cols-12")}
+              >
+                <Controller
+                  name="tradeName"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="tradeName">Nombre comercial</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="tradeName"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="slug"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="slug">Subdominio</FieldLabel>
+                      <Input
+                        className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                        aria-invalid={fieldState.invalid}
+                        id="slug"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="companyName"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="companyName">Razón social</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="companyName"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="taxId"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="taxId">CUIT</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="taxId"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="description"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-12" : "md:col-span-12")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="street">Descripción</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="description"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+              </FieldGroup>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col">
+                <h2 className="text-lg font-semibold">Dirección</h2>
+                <span className="text-muted-foreground text-sm">Actualizá la dirección de tu negocio</span>
+              </div>
+              <FieldGroup
+                className={cn("grid grid-cols-1 gap-6", open ? "md:grid-cols-1 lg:grid-cols-12" : "md:grid-cols-12")}
+              >
+                <Controller
+                  name="street"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="street">Calle</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="street"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="city"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="city">Ciudad</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="city"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="province"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="province">Provincia</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="province"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="country"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="country">País</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="country"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="zipCode"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="zipCode">Código postal</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="zipCode"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+              </FieldGroup>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col">
+                <h2 className="text-lg font-semibold">Contacto</h2>
+                <span className="text-muted-foreground text-sm">Actualiza los medios de contacto de tu negocio</span>
+              </div>
+              <FieldGroup
+                className={cn("grid grid-cols-1 gap-6", open ? "md:grid-cols-1 lg:grid-cols-12" : "md:grid-cols-12")}
+              >
+                <Controller
+                  name="email"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="email">Email</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="email"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="phoneNumber"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="phoneNumber">Número de teléfono</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="phoneNumber"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="whatsAppNumber"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      className={cn("col-span-1", open ? "md:col-span-12 lg:col-span-6" : "md:col-span-6")}
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="whatsAppNumber">Número de WhatsApp</FieldLabel>
+                      <Input
+                        aria-invalid={fieldState.invalid}
+                        id="whatsAppNumber"
+                        {...field}
+                        onChange={async (e) => {
+                          console.log(e);
+                        }}
+                      />
+                      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                  )}
+                />
+              </FieldGroup>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex items-center justify-between pt-4">
+          <div>
+            {/* {isLoadingAdmin && <Loader className="text-sm" size={18} text="Cargando administrador" />} */}
+            {/* {isLoadingRoles && <Loader className="text-sm" size={18} text="Cargando roles" />} */}
+          </div>
+          <div className="flex gap-4">
+            <Button variant="ghost" onClick={handleCancel}>
+              Cancelar
+            </Button>
+            <Button disabled={!form.formState.isDirty} form="edit-form" type="submit" variant="default">
+              {isSaving ? <Loader color="white" text="Guardando" /> : "Guardar"}
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
