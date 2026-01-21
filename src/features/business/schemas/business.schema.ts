f3bc4aed3e192b7039350ed79d027b537d7bdfae
alanloffler <alanmatiasloffler@gmail.com>
2026-01-20 22:17:20 -0300

    feat(business): update validations

--- src/features/business/schemas/business.schema.ts
+++ src/features/business/schemas/business.schema.ts
@@ -1,26 +1,61 @@
 import z from "zod";
 
 export const businessSchema = z.object({
-  slug: z.string().nonempty("El slug es obligatorio").min(3, "El slug debe tener al menos 3 caracteres"),
+  slug: z
+    .string()
+    .nonempty("El slug es obligatorio")
+    .min(3, "El slug debe tener al menos 3 caracteres")
+    .max(50, "El slug debe tener como máximo 50 caracteres"),
   taxId: z.string().nonempty("El CUIT no puede estar vacío").length(11, "El CUIT debe tener 11 dígitos"),
   companyName: z
     .string()
     .nonempty("La razón social es obligatoria")
     .min(3, "La razón social debe tener al menos 3 caracteres")
-    .max(50, "La razón social debe tener menos de 50 caracteres"),
+    .max(50, "La razón social debe tener como máximo 50 caracteres"),
   tradeName: z
     .string()
     .nonempty("El nombre comercial es obligatorio")
     .min(3, "El nombre comercial debe tener al menos 3 caracteres")
-    .max(50, "El nombre comercial debe tener menos de 50 caracteres"),
-  description: z.string().nonempty("La descripción es obligatoria"),
-  street: z.string().nonempty("La calle es obligatoria"),
-  city: z.string().nonempty("La ciudad es obligatoria"),
-  province: z.string().nonempty("La provincia es obligatoria"),
-  country: z.string().nonempty("El país es obligatorio"),
-  zipCode: z.string().nonempty("El código postal es obligatorio"),
+    .max(50, "El nombre comercial debe tener como máximo 50 caracteres"),
+  description: z
+    .string()
+    .nonempty("La descripción es obligatoria")
+    .min(3, "La descripción debe tener al menos 3 caracteres")
+    .max(200, "La descripción debe tener como máximo 200 caracteres"),
+  street: z
+    .string()
+    .nonempty("La calle es obligatoria")
+    .min(3, "La calle debe tener al menos 3 caracteres")
+    .max(50, "La calle debe tener como máximo 50 caracteres"),
+  city: z
+    .string()
+    .nonempty("La ciudad es obligatoria")
+    .min(3, "La ciudad debe tener al menos 3 caracteres")
+    .max(50, "La ciudad debe tener menos de 50 caracteres"),
+  province: z
+    .string()
+    .nonempty("La provincia es obligatoria")
+    .min(3, "La provincia debe tener al menos 3 caracteres")
+    .max(50, "La provincia debe tener como máximo 50 caracteres"),
+  country: z
+    .string()
+    .nonempty("El país es obligatorio")
+    .min(3, "El país debe tener al menos 3 caracteres")
+    .max(50, "El país debe tener como máximo 50 caracteres"),
+  zipCode: z
+    .string()
+    .nonempty("El código postal es obligatorio")
+    .min(4, "El código postal debe tener al 4 dígitos")
+    .max(6, "El código postal debe tener como máximo 6 dígitos"),
   email: z.email({ message: "Debes ingresar un email válido" }),
-  phoneNumber: z.string().nonempty("El número de teléfono es obligatorio"),
-  whatsAppNumber: z.string().optional(),
-  website: z.string().optional(),
+  phoneNumber: z
+    .string()
+    .nonempty("El número de teléfono es obligatorio")
+    .length(10, "El número de teléfono debe tener 10 dígitos"),
+  whatsAppNumber: z.string().length(10, "El número de WhatsApp debe tener 10 dígitos").optional(),
+  website: z
+    .string()
+    .min(7, "El sitio web debe tener al menos 7 caracteres")
+    .max(50, "El sitio web debe tener como máximo 50 caracteres")
+    .optional(),
 });
