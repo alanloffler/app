5de9c55bcff3ec2db02946ca7e781cb534ad8945
alanloffler <alanmatiasloffler@gmail.com>
2026-01-22 00:04:14 -0300

    styles(users): form responsive width

--- src/features/users/views/CreateUser.tsx
+++ src/features/users/views/CreateUser.tsx
@@ -9,7 +9,7 @@ export default function CreateUser() {
   const userRole = state.role;
 
   return (
-    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
+    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-full">
       {userRole === EUserRole["admin"] && <div>Mostrar formulario de creacion de admin</div>}
       {userRole === EUserRole["patient"] && <div>Mostrar formulario de creacion de paciente</div>}
       {userRole === EUserRole["professional"] && <CreateProfessionalForm />}
