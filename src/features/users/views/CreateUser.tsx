d8984af36dfe5dddaf24b0e6df90eb6162129cf3
alanloffler <alanmatiasloffler@gmail.com>
2026-01-21 21:38:04 -0300

    feat(users): render form by user type
    New approach to use custom forms by user type

--- src/features/users/views/CreateUser.tsx
+++ src/features/users/views/CreateUser.tsx
@@ -1,9 +1,18 @@
-import { CreateForm } from "@users/components/CreateForm";
+import { CreateProfessionalForm } from "@users/components/CreateProfessionalForm";
+
+import { useLocation } from "react-router";
+
+import { EUserRole } from "@roles/enums/user-role.enum";
 
 export default function CreateUser() {
+  const { state } = useLocation();
+  const userRole = state.role;
+
   return (
     <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
-      <CreateForm />
+      {userRole === EUserRole["admin"] && <div>Mostrar formulario de creacion de admin</div>}
+      {userRole === EUserRole["patient"] && <div>Mostrar formulario de creacion de paciente</div>}
+      {userRole === EUserRole["professional"] && <CreateProfessionalForm />}
     </div>
   );
 }
