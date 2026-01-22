9f40038ab6b2fbbc3706702881d08d1723881e56
alanloffler <alanmatiasloffler@gmail.com>
2026-01-22 00:03:58 -0300

    feat(users): create professional schema

--- src/features/users/schemas/create-professional.schema.ts
+++ src/features/users/schemas/create-professional.schema.ts
@@ -6,8 +6,35 @@ export const createProfessionalSchema = userSchema.extend({
     .string()
     .nonempty("La contraseña es obligatoria")
     .min(8, "La contraseña debe tener al menos 8 caracteres"),
-  // userId:
-  // licenseId:
-  // specialties:
-  // workingDays:
+  licenseId: z
+    .string()
+    .nonempty("La matrícula es obligatoria")
+    .min(3, "La matrícula debe tener al menos 3 caracteres")
+    .max(20, "La matrícula debe tener como máximo 20 caracteres"),
+  professionalPrefix: z
+    .string()
+    .nonempty("El prefijo profesional es obligatorio")
+    .min(3, "El prefijo profesional debe tener al menos 3 caracteres")
+    .max(20, "El prefijo profesional debe tener como máximo 20 caracteres"),
+  specialty: z
+    .string()
+    .nonempty("La especialidad es obligatoria")
+    .min(3, "La especialidad debe tener al menos 3 caracteres")
+    .max(20, "La especialidad debe tener como máximo 20 caracteres"),
+  workingDays: z
+    .any()
+    .refine(Array.isArray, {
+      message: "Los días laborales deben ser un array",
+    })
+    .pipe(
+      z
+        .array(
+          z
+            .any()
+            .refine((val) => Number.isInteger(val), { message: "Cada día laboral debe ser un número entero" })
+            .refine((val) => val >= 0, { message: "El día laboral mínimo es 0 (Domingo)" })
+            .refine((val) => val <= 6, { message: "El día máximo es 6 (Sábado)" }),
+        )
+        .nonempty("Debes agregar al menos un día laboral"),
+    ),
 });
