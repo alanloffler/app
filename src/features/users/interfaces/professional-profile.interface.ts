480e7097026b4e98ee9b52f93266cd24f4d6ba13
alanloffler <alanmatiasloffler@gmail.com>
2026-01-23 10:30:13 -0300

    feat(users): professional profile interface

---
+++ src/features/users/interfaces/professional-profile.interface.ts
@@ -0,0 +1,9 @@
+export interface IProfessionalProfile {
+  createdAt: string;
+  id: string;
+  licenseId: string;
+  professionalPrefix: string;
+  specialty: string;
+  updatedAt: string;
+  workingDays: string[];
+}
