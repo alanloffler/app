37a16f36b5a4cb99ee311c98ca5b0dd932e5159e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-23 18:57:08 -0300

    feat(users): handle edit form by id & role

--- src/features/users/views/EditUser.tsx
+++ src/features/users/views/EditUser.tsx
@@ -1,13 +1,23 @@
-import { EditForm } from "@users/components/EditForm";
+import { EditProfessionalForm } from "@users/components/EditProfessionalForm";
 
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
+
+import { EUserRole } from "@roles/enums/user-role.enum";
 
 export default function EditUser() {
+  const { state } = useLocation();
+  const userRole = state.role;
   const { id } = useParams();
 
+  console.log(userRole);
+
+  if (!id) return null;
+
   return (
-    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
-      <EditForm userId={id!} />
+    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[80%]">
+      {userRole === EUserRole["admin"] && <div>Mostrar formulario de edición de admin</div>}
+      {userRole === EUserRole["patient"] && <div>Mostrar formulario de edición de paciente</div>}
+      {userRole === EUserRole["professional"] && <EditProfessionalForm id={id} />}
     </div>
   );
 }
