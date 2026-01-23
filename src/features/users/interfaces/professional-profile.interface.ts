cdcf8a722921a0955d455bf33553a73bf81d1b8a
alanloffler <alanmatiasloffler@gmail.com>
2026-01-23 14:00:23 -0300

    feat(users): profile props startHour & endHour

--- src/features/users/interfaces/professional-profile.interface.ts
+++ src/features/users/interfaces/professional-profile.interface.ts
@@ -1,9 +1,11 @@
 export interface IProfessionalProfile {
   createdAt: string;
+  endHour: string;
   id: string;
   licenseId: string;
   professionalPrefix: string;
   specialty: string;
+  startHour: string;
   updatedAt: string;
   workingDays: string[];
 }
