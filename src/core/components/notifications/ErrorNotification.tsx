ec1680751760e3d8bb1afb3cf320fe49dad74290
alanloffler <alanmatiasloffler@gmail.com>
2026-01-23 14:13:18 -0300

    feat(components): tryAgain button conditional by prop

--- src/core/components/notifications/ErrorNotification.tsx
+++ src/core/components/notifications/ErrorNotification.tsx
@@ -4,18 +4,21 @@ import { Button } from "@components/ui/button";
 
 interface IProps {
   message: string;
+  tryAgain?: boolean;
 }
 
-export function ErrorNotification({ message }: IProps) {
+export function ErrorNotification({ message, tryAgain = true }: IProps) {
   return (
     <div className="mx-auto flex w-fit flex-col items-center gap-3 rounded-lg border border-red-200 bg-red-100 p-3 text-sm font-medium text-red-500 md:flex-row">
       <div className="flex items-center gap-3">
         <CircleX className="h-5 w-5 shrink-0" />
         <p>{message}</p>
       </div>
-      <Button onClick={() => window.location.reload()} size="sm" variant="destructive">
-        Intentálo de nuevo
-      </Button>
+      {tryAgain && (
+        <Button onClick={() => window.location.reload()} size="sm" variant="destructive">
+          Intentálo de nuevo
+        </Button>
+      )}
     </div>
   );
 }
