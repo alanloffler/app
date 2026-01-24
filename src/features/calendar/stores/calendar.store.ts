a4fa668714505c995602575b21b20ebfee90157e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-24 17:05:24 -0300

    feat(calendar): selectedProfessional & setSelectedProfessional

--- src/features/calendar/stores/calendar.store.ts
+++ src/features/calendar/stores/calendar.store.ts
@@ -1,12 +1,15 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+import type { IUser } from "@users/interfaces/user.interface";
 import type { TView } from "@calendar/interfaces/calendar-view.type";
 
 interface CalendarState {
   selectedDate: Date;
+  selectedProfessional: IUser | null;
   selectedView: TView;
   setSelectedDate: (date: Date) => void;
+  setSelectedProfessional: (user: IUser) => void;
   setSelectedView: (view: TView) => void;
 }
 
@@ -14,12 +17,14 @@ export const useCalendarStore = create(
   persist<CalendarState>(
     (set) => ({
       selectedDate: new Date(),
+      selectedProfessional: null,
       selectedView: "week",
       setSelectedDate: (date) => set({ selectedDate: date }),
+      setSelectedProfessional: (user) => set({ selectedProfessional: user }),
       setSelectedView: (view) => set({ selectedView: view }),
     }),
     {
-      name: "calendar",
+      name: "app-calendar",
       storage: createJSONStorage(() => sessionStorage),
       merge: (persistedState, currentState) => ({
         ...currentState,
