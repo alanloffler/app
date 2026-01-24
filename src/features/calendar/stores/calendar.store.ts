8da5c1dcb2e6a1e4062041780f002b73ec5388d6
alanloffler <alanmatiasloffler@gmail.com>
2026-01-24 19:08:21 -0300

    feat(calendar): professional calendar config

--- src/features/calendar/stores/calendar.store.ts
+++ src/features/calendar/stores/calendar.store.ts
@@ -1,15 +1,18 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+import type { ICalendarConfig } from "@calendar/interfaces/calendar-config.interface";
 import type { IUser } from "@users/interfaces/user.interface";
 import type { TView } from "@calendar/interfaces/calendar-view.type";
 
 interface CalendarState {
   selectedDate: Date;
   selectedProfessional: IUser | null;
+  selectedProfessionalConfig: ICalendarConfig | undefined;
   selectedView: TView;
   setSelectedDate: (date: Date) => void;
   setSelectedProfessional: (user: IUser) => void;
+  setSelectedProfessionalConfig: (config: ICalendarConfig) => void;
   setSelectedView: (view: TView) => void;
 }
 
@@ -18,21 +21,30 @@ export const useCalendarStore = create(
     (set) => ({
       selectedDate: new Date(),
       selectedProfessional: null,
+      selectedProfessionalConfig: undefined,
       selectedView: "week",
       setSelectedDate: (date) => set({ selectedDate: date }),
       setSelectedProfessional: (user) => set({ selectedProfessional: user }),
+      setSelectedProfessionalConfig: (config) => set({ selectedProfessionalConfig: config }),
       setSelectedView: (view) => set({ selectedView: view }),
     }),
     {
       name: "app-calendar",
       storage: createJSONStorage(() => sessionStorage),
-      merge: (persistedState, currentState) => ({
-        ...currentState,
-        ...(persistedState as CalendarState),
-        selectedDate: (persistedState as CalendarState)?.selectedDate
-          ? new Date((persistedState as CalendarState).selectedDate)
-          : currentState.selectedDate,
-      }),
+      partialize: (state) =>
+        ({
+          selectedDate: state.selectedDate,
+          selectedView: state.selectedView,
+        }) as CalendarState,
+      merge: (persistedState, currentState) => {
+        const persisted = persistedState as Partial<CalendarState>;
+
+        return {
+          ...currentState,
+          ...persisted,
+          selectedDate: persisted?.selectedDate ? new Date(persisted.selectedDate) : currentState.selectedDate,
+        };
+      },
     },
   ),
 );
