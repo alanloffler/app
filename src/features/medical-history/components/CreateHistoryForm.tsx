6ed7a5703f21d4595239df89917b8b27b5b84c1d
alanloffler <alanmatiasloffler@gmail.com>
2026-01-27 19:10:17 -0300

    feat(medical-history): create form

---
+++ src/features/medical-history/components/CreateHistoryForm.tsx
@@ -0,0 +1,11 @@
+export function CreateHistoryForm() {
+  return (
+    <form className="flex flex-col gap-5">
+      <div>Motivo de consulta:</div>
+      <div>Diagnóstico:</div>
+      <div>Tratamiento</div>
+      <div>Receta:</div>
+      <div>Notas adicionales</div>
+    </form>
+  );
+}
