c81a16e8bdfada1f43f9e2f5bd911d008608b391
alanloffler <alanmatiasloffler@gmail.com>
2026-02-03 17:38:07 -0300

    feat(users): exported create professional interface

---
+++ src/features/users/interfaces/create-professional.interface.ts
@@ -0,0 +1,18 @@
+export interface ICreateProfessionalForm {
+  dailyExceptionEnd: string;
+  dailyExceptionStart: string;
+  email: string;
+  endHour: string;
+  firstName: string;
+  ic: string;
+  lastName: string;
+  licenseId: string;
+  password: string;
+  phoneNumber: string;
+  professionalPrefix: string;
+  slotDuration: string;
+  specialty: string;
+  startHour: string;
+  userName: string;
+  workingDays: number[];
+}
