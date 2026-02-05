9c99b4ae4401880e2ae4b92e50aaf31c30f9d93b
alanloffler <alanmatiasloffler@gmail.com>
2026-02-05 20:32:03 -0300

    feat(users): create patient interface

---
+++ src/features/users/interfaces/create-patient.interface.ts
@@ -0,0 +1,16 @@
+export interface ICreatePatientForm {
+  birthDay: Date;
+  bloodType: string;
+  email: string;
+  emergencyContactName: string;
+  emergencyContactPhone: string;
+  firstName: string;
+  gender: string;
+  height: number;
+  ic: string;
+  lastName: string;
+  password: string;
+  phoneNumber: string;
+  userName: string;
+  weight: number;
+}
