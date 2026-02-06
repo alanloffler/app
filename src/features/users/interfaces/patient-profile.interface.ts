4c8bcc7b3ac09aff233ed31d294c279251856cea
alanloffler <alanmatiasloffler@gmail.com>
2026-02-06 18:31:43 -0300

    feat(users): patient profile interface

---
+++ src/features/users/interfaces/patient-profile.interface.ts
@@ -0,0 +1,12 @@
+export interface IPatientProfile {
+  birthDay: Date;
+  bloodType: string;
+  createdAt: Date;
+  emergencyContactName: string;
+  emergencyContactPhone: string;
+  gender: string;
+  height: number;
+  id: string;
+  updatedAt: Date;
+  weight: number;
+}
