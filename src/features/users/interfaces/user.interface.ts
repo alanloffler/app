f66dd0163d7b18dc93de2c05fef3b20a861562a8
alanloffler <alanmatiasloffler@gmail.com>
2026-02-06 18:31:22 -0300

    feat(users): patientProfile prop

--- src/features/users/interfaces/user.interface.ts
+++ src/features/users/interfaces/user.interface.ts
@@ -1,4 +1,5 @@
 import type { IMedicalHistory } from "@users/interfaces/medical-history.interface";
+import type { IPatientProfile } from "@users/interfaces/patient-profile.interface";
 import type { IProfessionalProfile } from "@users/interfaces/professional-profile.interface";
 import type { IRole } from "@roles/interfaces/role.interface";
 
@@ -13,6 +14,7 @@ export interface IUser {
   medicalHistory?: IMedicalHistory[];
   password?: string;
   phoneNumber: string;
+  patientProfile?: IPatientProfile;
   professionalProfile?: IProfessionalProfile;
   role: IRole;
   roleId: string;
