21df698486e1ceaed26d12676dfa8110bf882417
alanloffler <alanmatiasloffler@gmail.com>
2026-02-07 18:53:35 -0300

    feat(medical-history): onCreated medical history fn

--- src/features/medical-history/components/sheets/CreateHistorySheet.tsx
+++ src/features/medical-history/components/sheets/CreateHistorySheet.tsx
@@ -7,11 +7,12 @@ import type { IUser } from "@users/interfaces/user.interface";
 
 interface IProps {
   user: IUser;
+  onCreated: () => void;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export function CreateHistorySheet({ user, open, setOpen }: IProps) {
+export function CreateHistorySheet({ user, onCreated, open, setOpen }: IProps) {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetContent className="sm:min-w-[480px]">
@@ -22,7 +23,7 @@ export function CreateHistorySheet({ user, open, setOpen }: IProps) {
           </SheetDescription>
         </SheetHeader>
         <div className="flex flex-col gap-6 p-4">
-          <CreateHistoryForm user={user} setOpen={setOpen} />
+          <CreateHistoryForm user={user} onCreated={onCreated} setOpen={setOpen} />
         </div>
       </SheetContent>
     </Sheet>
