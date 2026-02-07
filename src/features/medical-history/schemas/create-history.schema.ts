88012e5091c3eba605fd0eb52ac5d7e083ad8cb9
alanloffler <alanmatiasloffler@gmail.com>
2026-02-06 21:22:05 -0300

    feat(medical-history): create history schema

---
+++ src/features/medical-history/schemas/create-history.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const createHistorySchema = z.object({
+  businessId: z
+    .uuidv4({ message: "El id de la empresa es obligatorio" })
+    .nonoptional("El id del negocio es obligatorio"),
+  comments: z.string().min(3, "Mínimo 3 caracteres").max(1000, "Máximo 1000 caracteres").nonoptional(),
+  eventId: z.uuidv4({ message: "El id del usuario es obligatorio" }).optional(),
+  reason: z
+    .string()
+    .nonempty("El motivo de consulta es obligatorio")
+    .min(3, "Mínimo 3 caracteres")
+    .max(100, "Máximo 100 caracteres"),
+  recipe: z.boolean({ message: "La receta es obligatoria" }).nonoptional("La receta es obligatoria"),
+  userId: z.uuidv4({ message: "El id del usuario es obligatorio" }).nonoptional("El id del usuario es obligatorio"),
+});
