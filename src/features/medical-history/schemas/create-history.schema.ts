ea3808496b78dff980c76d6668c7725c2b2b1ddc
alanloffler <alanmatiasloffler@gmail.com>
2026-02-06 23:33:58 -0300

    feat(medical-history): date prop

--- src/features/medical-history/schemas/create-history.schema.ts
+++ src/features/medical-history/schemas/create-history.schema.ts
@@ -5,6 +5,7 @@ export const createHistorySchema = z.object({
     .uuidv4({ message: "El id de la empresa es obligatorio" })
     .nonoptional("El id del negocio es obligatorio"),
   comments: z.string().min(3, "Mínimo 3 caracteres").max(1000, "Máximo 1000 caracteres").nonoptional(),
+  date: z.date({ message: "La fecha es obligatoria" }),
   eventId: z.uuidv4({ message: "El id del usuario es obligatorio" }).optional(),
   reason: z
     .string()
